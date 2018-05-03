687251cba279521689770cfe0e38d6b35285c1a3
Timur Lyashev <Timur.Lyashev@gmail.com>
2018-05-03 17:06:31 +0300

    Add Svn (branch and tags fixes)

--- src/common/source.cpp
+++ src/common/source.cpp
@@ -1033,15 +1033,12 @@ void Svn::download() const
 {
     downloadRepository([this]()
     {
-        Command::execute({ "svn", "checkout", url });
-
-        String branchPath = url.substr(url.find_last_of("/") + 1);
-        ScopedCurrentPath scp(current_thread_path() / branchPath);
+        Command::execute({ "svn", "checkout", url + "/trunk"});
 
         if (!tag.empty())
-            Command::execute({ "svn", "sw",  branchPath }); //tag
+            Command::execute({ "svn", "sw", url + "/tags" + tag}); //tag
         else if (!branch.empty())
-            Command::execute({ "svn", "sw",  branchPath }); //branch
+            Command::execute({ "svn", "sw", url + "/branches" + branch}); //branch
         else if (revision != -1) 
             Command::execute({ "svn", "up", "-r" + revision });
     });
